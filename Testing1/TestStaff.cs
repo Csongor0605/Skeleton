65588a15baa9d368194ace3d7148da4fb0c2beb5
Csongor0605 <csongor0605@gmail.com>
2023-03-04 13:49:29 +0000

    Created class clsStaff and TestStaff, Test currently Empty

---
+++ Testing1/TestStaff.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class TestBook
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            
+        }
+    }
+}
