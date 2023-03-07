54d9247496e1be1bf35da466c65a426b9aca11a7
Morgan McCabe <P2660135@my365.dmu.ac.uk>
2023-03-07 16:41:45 +0000

    modified MorganDataEntry btnOk_Click method to created clsBook instance, and pass data enterted in textbox of data entry windowto veiwer window.

--- AdminSystem/MorganDataEntry.aspx.cs
+++ AdminSystem/MorganDataEntry.aspx.cs
@@ -8,13 +8,42 @@ using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
+    
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+        //create instance of clsBook, class in classLibrary
+        clsBook ABook = new clsBook();
+
+        //capture BookID from BookID textboc in data entry page
+        ABook.BookID = int.Parse(txtbxBookId.Text);
+
+        // capture BookTitle from BookTitle textboc in data entry page
+        ABook.BookTitle = txtbxBookTitle.Text;
+
+        //capture Book author, from Author Textbox in Data Entry window
+        ABook.Author = txtbxAuthor.Text;
+
+        //capture Book Genre, from Genre Textbox in Data Entry window
+        ABook.Genre = txtbxGenre.Text;
+
+        //capture Book Quantity, from Quantity Textbox in Data Entry window
+        ABook.Quantity = int.Parse(txtbxQuantity.Text);
+
+        //capture Book restockordered (Booleans), from checkbox in data entry window
+        ABook.RestockOrdered = chbxRestockOrdered.Checked;
+
+        //capture Book Restock date of Arrival (Datetime), from textbox in data entry window
+        ABook.Restock_date = DateTime.Parse(txtbxRestock_date.Text);
+
+        //capture Book restock Quantity from textbox in Data Entry window
+        ABook.RestockOrder_Quantity = int.Parse(txtbxRestockOrder_Quantity.Text);
+
+        // store address is session object
+        Session["ABook"] = ABook;
+        //Navigate to viewer page
         Response.Redirect("MorganViewer.aspx");
     }
+
+  
 }
