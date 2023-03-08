a4b9f0c71795edc9a8c8b77f51553718bf63154a
Faisal Alanzi <P2724748@my365.dmu.ac.uk>
2023-03-08 10:42:27 +0000

    added testing for the complaint class

---
+++ Testing5/tstComplaints.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstComplaints
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // creat an instance of the class we want to creat
+            clsComplaint Complaint = new clsComplaint();
+            // test to see it exsits 
+            Assert.IsNotNull(Complaint);
+        }
+    }
+}
