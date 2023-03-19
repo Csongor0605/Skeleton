75c6d7a88443c9aa5d8548add5e1d2cdeb636e23
Faisal Alanzi <P2724748@my365.dmu.ac.uk>
2023-03-19 22:00:14 +0000

    added a presentation layer to the customer support page

--- AdminSystem/FaisalDataEntry.aspx.cs
+++ AdminSystem/FaisalDataEntry.aspx.cs
@@ -4,11 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+
+  
+
+        //create a new instance of the clsComplaint
+        clsComplaint Complaint = new clsComplaint();
+
+        //capture the order id
+        Complaint.OrderID = Convert.ToInt32(TxtOrderID.Text);
+
+        //capture the name
+        Complaint.Name = txtName.Text;
+
+        //capture the subject
+        Complaint.Subject = txtSubject.Text;
+
+        //capture the email
+        Complaint.Email = txtEmail.Text;
+        
+        //capture the complaint
+        Complaint.Text = txtComplaint.Text;
+
+        //capture the date
+        Complaint.DateAdded = Convert.ToDateTime(txtDate.Text);
+        
+        //store the compalint in the session box
+        Session["Complaint"] = Complaint;
+
+        //take you to the viewer page
+        Response.Redirect("FaisalViewer.aspx");
+
+
+
+ 
+        
+     
     }
 }
